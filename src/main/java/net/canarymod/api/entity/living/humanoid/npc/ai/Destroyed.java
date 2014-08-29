a7769f36f7a970197b2f297d24c45c78eeeba165
darkdiplomat <darkdiplomat@visualillusionsent.net>
2014-08-28 19:20:57 -0500

    we'll just fix this before somners notices...

--- src/main/java/net/canarymod/api/entity/living/humanoid/npc/ai/Destroyed.java
+++ src/main/java/net/canarymod/api/entity/living/humanoid/npc/ai/Destroyed.java
@@ -3,7 +3,7 @@ package net.canarymod.api.entity.living.humanoid.npc.ai;
 /**
  * @author Jason (darkdiplomat)
  */
-public final class Destoryed extends NPCAI {
+public final class Destroyed extends NPCAI {
 
     public final String toString() {
         return "NPCAI.Destroyed[]";
