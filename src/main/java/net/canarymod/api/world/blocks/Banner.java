3b86e4ec4c0558997e32bd46ed9c232e98e3c46b
darkdiplomat <darkdiplomat@visualillusionsent.net>
2014-10-02 10:22:41 -0500

    Banner tile entity

---
+++ src/main/java/net/canarymod/api/world/blocks/Banner.java
@@ -0,0 +1,10 @@
+package net.canarymod.api.world.blocks;
+
+/**
+ * Banner TileEntity wrapper
+ *
+ * @author Jason Jones (darkdiplomat)
+ */
+public interface Banner extends TileEntity {
+    //TODO WIP
+}
