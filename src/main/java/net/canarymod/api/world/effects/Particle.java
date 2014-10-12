55ccc3c34ce4219aac13ad42d45f76cd829d6414
darkdiplomat <darkdiplomat@visualillusionsent.net>
2014-10-12 14:14:10 -0500

    updated Particle types (new ones and matching the enum to the vanilla enum in name and position)

--- src/main/java/net/canarymod/api/world/effects/Particle.java
+++ src/main/java/net/canarymod/api/world/effects/Particle.java
@@ -3,48 +3,53 @@ package net.canarymod.api.world.effects;
 /**
  * A particle that can be spawned in the world.
  *
- * @author Chris Ksoll
+ * @author Chris Ksoll (damagefilter)
+ * @author Jason Jones (darkdiplomat)
  */
 public class Particle {
     public enum Type {
-        SNOWBALLSPLASH("snowballspoof"), //
-        PORTAL("portal"), //
-        WATERSPLASH("splash"), //
-        WATERBUBBLES("bubble"), //
-        EXPLOSION("hugeexplosion"), //
-        FLAME("flame"), //
-        HEART("heart"), //
-        SMOKE("smoke"), //
-        LARGESMOKE("largesmoke"), //
-        CRITICALHITSPARK("crit"), //
-        MAGICCRITICAL("magicCrit"), //
-        NOTE("note"), //
-        MAGICRUNES("enchantmenttable"), //
-        LAVASPARK("lava"), //
-        WATERDRIP("dripWater"), //
-        LAVADRIP("dripLava"), //
-        SLIMESPLATTER("slime"), //
-        REDSTONEFUMES("reddust"), //
-        MYCELIUMSPORES("townaura"), //
-        FIREWORKSPARK("fireworksSpark"), //
-        SUSPENDED("suspended"), //
-        MOBSPELL("mobSpell"), //
-        MOBSPELLAMBIENT("mobSpellAmbient"), //
-        SPELL("spell"), //
-        INSTANTSPELL("instantSpell"), //
-        WITCHMAGIC("witchMagic"), //
-        LAVA("lava"), //
-        FOOTSTEP("footstep"), //
-        SNOWSHOVEL("snowshovel"), //
-        ANGRYVILLAGER("angryVillager"), //
-        HAPPYVILLAGER("happyVillager"), //
+        EXPLOSION_NORMAL("explode"),
+        EXPLOSION_LARGE("largeexplode"),
+        EXPLOSION_HUGE("hugeexplosion"),
+        FIREWORKS_SPARK("fireworksSpark"),
+        WATER_BUBBLE("bubble"),
+        WATER_SPLASH("splash"),
+        WATER_WAKE("wake"),
+        SUSPENDED("suspended"),
+        SUSPENDED_DEPTH("depthsuspend"),
+        CRIT("crit"),
+        CRIT_MAGIC("magicCrit"),
+        SMOKE_NORMAL("smoke"),
+        SMOKE_LARGE("largesmoke"),
+        SPELL("spell"),
+        SPELL_INSTANT("instantSpell"),
+        SPELL_MOB("mobSpell"),
+        SPELL_MOB_AMBIENT("mobSpellAmbient"),
+        SPELL_WITCH("witchMagic"),
+        DRIP_WATER("dripWater"),
+        DRIP_LAVA("dripLava"),
+        VILLAGER_ANGRY("angryVillager"),
+        VILLAGER_HAPPY("happyVillager"),
+        TOWN_AURA("townaura"),
+        NOTE("note"),
+        PORTAL("portal"),
+        ENCHANTMENT_TABLE("enchantmenttable"),
+        FLAME("flame"),
+        LAVA("lava"),
+        FOOTSTEP("footstep"),
+        CLOUD("cloud"),
+        REDSTONE("reddust"),
+        SNOWBALL("snowballpoof"),
+        SNOW_SHOVEL("snowshovel"),
+        SLIME("slime"),
+        HEART("heart"),
         BARRIER("barrier"),
-        //
-        DROPLET("droplet"),
-        //
-        MOBAPPEARANCE("mobAppearance"),
-        TAKE("take"),
-        //
+        ITEM_CRACK("iconcrack_"),
+        BLOCK_CRACK("blockcrack_"),
+        BLOCK_DUST("blockdust_"),
+        WATER_DROP("droplet"),
+        ITEM_TAKE("take"),
+        MOB_APPEARANCE("mobappearance"),
 
         ;
 
