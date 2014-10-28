f7569005105d8bb1cf89f095f8f5a6f28751d9f9
darkdiplomat <darkdiplomat@visualillusionsent.net>
2014-10-28 15:15:35 -0500

    fixing a rare stack overflow error withing the CanaryClassWatcher/CanaryClassLoader caused by a circular class lookup updated .gitignore

--- src/main/java/net/canarymod/CanaryClassWatcher.java
+++ src/main/java/net/canarymod/CanaryClassWatcher.java
@@ -18,31 +18,38 @@ final class CanaryClassWatcher {
     /**
      * Finds a loaded {@link Class} from any of the {@link Plugin}'s {@link CanaryClassLoader}
      *
+     * @param looking
+     *         the {@link net.canarymod.CanaryClassLoader} looking for a {@link java.lang.Class}
      * @param name
      *         the name of the {@link Class}
      *
      * @return the {@link Class} if found; {@code null} otherwise
      */
-    synchronized final Class<?> findLoadedClass(String name) {
+    synchronized final Class<?> findLoadedClass(CanaryClassLoader looking, String name) {
         for (Class<?> cls : loadedClasses.values()) {
             if (cls.getName().equals(name)) {
                 return cls;
             }
         }
-        return loadClass(name); // ClassNotFound, attempt to load it
+        return loadClass(looking, name); // ClassNotFound, attempt to load it
     }
 
     /**
      * Loads a {@link Class} from the first loader that contains the class
      *
+     * @param looking
+     *         the {@link net.canarymod.CanaryClassLoader} looking for a {@link java.lang.Class}
      * @param name
      *         the name of the {@link Class} to be loaded
      *
      * @return the {@link Class} if found; {@code null} otherwise
      */
-    private synchronized final Class<?> loadClass(String name) {
+    private synchronized Class<?> loadClass(CanaryClassLoader looking, String name) {
         String nameTemp = name.replace('.', '/').concat(".class");
         for (CanaryClassLoader loader : loadedClasses.keySet()) {
+            if (loader == looking) {
+                continue; // We already know that this loader has failed
+            }
             if (loader.getResource(nameTemp) != null) {
                 try {
                     Class<?> cls = loader.loadClass(name);
