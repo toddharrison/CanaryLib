77ce57dd48a09175e6314c97f57016df327425ca
darkdiplomat <darkdiplomat@visualillusionsent.net>
2015-02-15 12:49:53 -0600

    adding world and base position to PortalCreate/PortalDestroy hooks

--- src/main/java/net/canarymod/hook/world/PortalCreateHook.java
+++ src/main/java/net/canarymod/hook/world/PortalCreateHook.java
@@ -1,25 +1,37 @@
 package net.canarymod.hook.world;
 
+import net.canarymod.api.world.World;
 import net.canarymod.api.world.blocks.Block;
+import net.canarymod.api.world.position.Position;
 import net.canarymod.hook.CancelableHook;
 
 /**
- * PortalCreateHook<br>
+ * PortalCreateHook<br/>
  * Contains information about a portal being created
  *
  * @author Chris (damagefilter)
+ * @author Jason Jones (darkdiplomat)
  */
 public final class PortalCreateHook extends CancelableHook {
 
-    private Block[][] blocks;
+    private final Block[][] blocks;
+    private final Position pos;
+    private final World world;
 
     /**
      * Constructs a new PortalCreateHook
      *
-     * @param blocks the {@link Block} set the make up the portal
+     * @param blocks
+     *         the {@link Block} set the make up the portal
+     * @param pos
+     *         the {@link net.canarymod.api.world.position.Position} where the portal is started
+     * @param world
+     *         the {@link World} the portal is being made in
      */
-    public PortalCreateHook(Block[][] blocks) {
+    public PortalCreateHook(Block[][] blocks, Position pos, World world) {
         this.blocks = blocks;
+        this.pos = pos;
+        this.world = world;
     }
 
     /**
@@ -31,8 +43,26 @@ public final class PortalCreateHook extends CancelableHook {
         return blocks;
     }
 
+    /**
+     * Gets the {@link net.canarymod.api.world.position.Position} where the portal is started
+     *
+     * @return position
+     */
+    public Position getPosition() {
+        return this.pos;
+    }
+
+    /**
+     * Gets the world the portal will be in
+     *
+     * @return the world
+     */
+    public World getWorld() {
+        return this.world;
+    }
+
     @Override
     public final String toString() {
-        return String.format("%s[Blocks=%s]", getHookName(), blocks);
+        return String.format("%s[Blocks=%s, World=%s]", getHookName(), blocks, world.getFqName());
     }
 }
--- src/main/java/net/canarymod/hook/world/PortalDestroyHook.java
+++ src/main/java/net/canarymod/hook/world/PortalDestroyHook.java
@@ -1,25 +1,32 @@
 package net.canarymod.hook.world;
 
+import net.canarymod.api.world.World;
 import net.canarymod.api.world.blocks.Block;
+import net.canarymod.api.world.position.Position;
 import net.canarymod.hook.CancelableHook;
 
 /**
- * PortalDestroyHook<br>
+ * PortalDestroyHook<br/>
  * Contains information about a portal being destroyed
  *
  * @author Chris (damagefilter)
+ * @author Jason Jones (darkdiplomat)
  */
 public final class PortalDestroyHook extends CancelableHook {
 
-    private Block[][] blocks;
+    private final Block[][] blocks;
+    private final Position pos;
+    private final World world;
 
     /**
      * Constructs a new PortalDestroyHook
      *
      * @param blocks the {@link Block} set the make up the portal
      */
-    public PortalDestroyHook(Block[][] blocks) {
+    public PortalDestroyHook(Block[][] blocks, Position pos, World world) {
         this.blocks = blocks;
+        this.pos = pos;
+        this.world = world;
     }
 
     /**
@@ -29,6 +36,24 @@ public final class PortalDestroyHook extends CancelableHook {
      */
     public Block[][] getBlockSet() {
         return blocks;
+    }
+
+    /**
+     * Gets the {@link net.canarymod.api.world.position.Position} where the portal is being destroyed
+     *
+     * @return position
+     */
+    public Position getPosition() {
+        return this.pos;
+    }
+
+    /**
+     * Gets the world the portal was
+     *
+     * @return the world
+     */
+    public World getWorld() {
+        return this.world;
     }
 
     @Override
